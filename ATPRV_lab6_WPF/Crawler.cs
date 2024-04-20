23d71f6eeeebd108c9960d57496cb3ebe02664ef
Arseniy <cenz1217@gmail.com>
2024-04-20 12:40:20 +0500

    Добавьте файлы проекта.

---
+++ ATPRV_lab6_WPF/Crawler.cs
@@ -0,0 +1,122 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATPRV_Lab6
+{
+    class Crawler
+    {
+        private readonly int _maxDepth;
+        private readonly HttpClient _httpClient;
+        private HashSet<string> _visitedUrls;
+        private List<(string, string)> _images;
+        private int _nodeCount;
+        private int _leafCount;
+        private int _maxHeight;
+        private int _currentDepth;
+
+        public Crawler(int maxDepth)
+        {
+            _maxDepth = maxDepth;
+            _httpClient = new HttpClient();
+            _visitedUrls = new HashSet<string>();
+            _images = new List<(string, string)>();
+            _nodeCount = 0;
+            _leafCount = 0;
+            _maxHeight = 0;
+            _currentDepth = 0;
+        }
+
+        public async Task CrawlAsync(string url, int depth = 0)
+        {
+            if (depth > _maxDepth) return;
+
+            if (_visitedUrls.Contains(url)) return;
+
+            _visitedUrls.Add(url);
+
+            _nodeCount++;
+            _currentDepth = Math.Max(_currentDepth, depth);
+            _maxHeight = Math.Max(_maxHeight, _currentDepth);
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+
+                // Проверяем, было ли перенаправление
+                if (response.StatusCode == System.Net.HttpStatusCode.MovedPermanently)
+                {
+                    var newUrl = response.Headers.Location.AbsoluteUri;
+                    await CrawlAsync(newUrl, depth);
+                    return;
+                }
+
+                var html = await response.Content.ReadAsStringAsync();
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+
+                // Получаем все изображения на странице
+                var imageNodes = htmlDocument.DocumentNode.SelectNodes("//img[@src]");
+                if (imageNodes != null)
+                {
+                    foreach (var imageNode in imageNodes)
+                    {
+                        string imageUrl = imageNode.Attributes["src"].Value;
+                        _images.Add((url, imageUrl));
+                    }
+                }
+
+                // Получаем ссылки на другие страницы и рекурсивно обходим их
+                var linkNodes = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+                if (linkNodes != null)
+                {
+                    var tasks = new List<Task>();
+                    foreach (var linkNode in linkNodes)
+                    {
+                        string nextUrl = linkNode.Attributes["href"].Value;
+                        if (Uri.TryCreate(new Uri(url), nextUrl, out Uri resultUri) &&
+                            (resultUri.Scheme == Uri.UriSchemeHttp || resultUri.Scheme == Uri.UriSchemeHttps))
+                        {
+                            tasks.Add(CrawlAsync(resultUri.ToString(), depth + 1));
+                        }
+                    }
+                    await Task.WhenAll(tasks);
+                }
+
+                if (depth == _maxDepth)
+                {
+                    _leafCount++;
+                    DisplayTreeInfo();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public string DisplayTreeInfo()
+        {
+            return $"Высота дерева: {_maxHeight}\n" +
+                $"Количество узлов: {_nodeCount}\n" +
+                $"Количество листьев: {_leafCount}";
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+        }
+
+        public void DisplayResults()
+        {
+            Console.WriteLine("Собранные изображения:");
+            foreach (var image in _images)
+            {
+                Console.WriteLine($"Страница: {image.Item1}, Изображение: {image.Item2}");
+            }
+        }
+
+    }
+}
---
+++ ATPRV_lab6_WPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ATPRV_Lab6;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ATPRV_lab6_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private async void StartCrawlButton_Click(object sender, RoutedEventArgs e)
+        {
+            string rootUrl = URLTextBox.Text;
+            int maxDepth = int.Parse(DepthTextBox.Text);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var crawler = new Crawler(maxDepth);
+            await crawler.CrawlAsync(rootUrl);
+
+            stopwatch.Stop();
+            TimeLabel.Content = $"Время работы: {stopwatch.ElapsedMilliseconds} мс";
+            TreeInfoLabel.Content =  crawler.DisplayTreeInfo();
+            //crawler.DisplayResults();
+        }
+    }
+}
